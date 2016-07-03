d7e38f4f7978d4fcd40fb4e407bc12706ac442e9
Truth <truthwzl@hotmail.com>
2016-07-03 22:59:51 +0800

    android ->2.1.3,ios->2.2.1

--- scripts/clone_pingpp_sdk.js
+++ scripts/clone_pingpp_sdk.js
@@ -11,14 +11,18 @@ module.exports = function(ctx) {
     , platform = ctx.opts.plugin.platform
     , sdk_path = path.join(ctx.opts.plugin.dir, 'sdk', platform)
     , sdk_url = 'https://github.com/PingPlusPlus/pingpp-'+platform+'.git';
-
+  var tag ="",tag2="-b"; 
+  if(platform=='android')
+	  tag = "2.1.3";
+  else if(platform=='ios')
+	  tag ="2.2.1";
   if (shell.test('-e', sdk_path)) { d.resolve(); }
   else {
     if (!shell.which('git')) d.reject('Can not request git');
 
     shell.echo(style.cyan.open + 'Cloning ping++ ' + platform + ' SDK, Please wait...' + style.cyan.close);
 
-    shell.exec(['git', 'clone', sdk_url, sdk_path].join(' '), {stdio: 'inherit'}).code == 0
+    shell.exec(['git','clone',tag2,tag,sdk_url, sdk_path].join(' '), {stdio: 'inherit'}).code == 0
       ? d.resolve()
       : d.reject('Clone ' + platform + ' SDK failed!');
   }
